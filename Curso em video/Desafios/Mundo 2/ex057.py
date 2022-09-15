acdc5d3cebe0f5635928970c774bcf3389f947bb
Arthur <72945155+ArthurVitor@users.noreply.github.com>
2022-09-15 20:28:17 -0300

    Update ex057.py

--- Curso em video/Desafios/Mundo 2/ex057.py
+++ Curso em video/Desafios/Mundo 2/ex057.py
@@ -1,4 +1,3 @@
 sx = str(input('Digite o sexo com [MF]: ')).strip().upper()[0]
 while sx not in 'MF':
-    if sx not in 'MF':
-        sx = str(input('Use somente [MF]: ')).strip().upper()[0]
+    sx = str(input('Use somente [MF]: ')).strip().upper()[0]
